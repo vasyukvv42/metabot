9624ce4929da530acd1c68608f64ec3e1f247271
Vladislav Vasyuk <vasyukvv42@gmail.com>
2020-05-13 00:10:04 +0300

    Update dockerfiles

--- modules/feedback/feedback/config.py
+++ modules/feedback/feedback/config.py
@@ -6,7 +6,6 @@ config = Config('.env')
 
 MODULE_URL = config('MODULE_URL')
 METABOT_URL = config('METABOT_URL')
-ADMIN_CHANNEL = config('ADMIN_CHANNEL')
 MONGODB_URI = config('MONGODB_URI')
 FEEDBACK_COLLECTION = config('FEEDBACK_COLLECTION', default='feedback')
 
